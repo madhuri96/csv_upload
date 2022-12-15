2c64db0a03f32183a984685070821638949d683c
madhuri96 <34019077+madhuri96@users.noreply.github.com>
2022-12-15 16:59:49 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,9 @@
 /****************IMPORTING PACKAGE*******************************/
 const mongoose = require("mongoose");
 
-let MONGO_URL = 'mongodb+srv://Madhuri12:root@cluster0.ux5jnqa.mongodb.net/csv_upload_db?retryWrites=true&w=majority';
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL);
 
 /*******************MAKING CONNECTION***************************/
-//mongoose.connect("mongodb+srv://Madhuri12:root@cluster0.ux5jnqa.mongodb.net/csv_upload_db?retryWrites=true&w=majority");  //mongodb://localhost/CSV
+mongoose.connect("mongodb+srv://Madhuri12:root@cluster0.ux5jnqa.mongodb.net/csv_upload_db?retryWrites=true&w=majority");  //mongodb://localhost/CSV
 
 //setting it to db
 const db = mongoose.connection;
